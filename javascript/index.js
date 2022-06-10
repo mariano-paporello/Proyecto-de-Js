13a7037023fb4169ac4abbdeb7a3ae54b1871031
Mariano Paporello <mariano.paporello@gmail.com>
2022-06-10 18:45:08 -0300

    Añadiendo productos y haciendo cambios en el tamaño de los mismos

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,4 @@
-const stock = [producto1, producto2, producto3, producto4, producto5];
+const stock = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9];
 let carrito = []
 
 let divContainer = document.querySelector(".productosLista")
